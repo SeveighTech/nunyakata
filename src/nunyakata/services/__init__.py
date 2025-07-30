edc58f3eb67190342cd97abe0ae571d5072c568d
Owbird <owbirdphorccuo@gmail.com>
2025-07-30 19:46:59 +0000

    feat (services): Add appsNmobile

--- src/nunyakata/services/__init__.py
+++ src/nunyakata/services/__init__.py
@@ -3,7 +3,9 @@ Service modules for various Ghana-based APIs.
 """
 
 from .nalo_solutions import NaloSolutions
+from .appsNmobile.apps_n_mobile import AppsNmobileOrchard
 
 __all__ = [
     "NaloSolutions",
+    "AppsNmobileOrchard",
 ]
